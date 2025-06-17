a07cfc80d638d154cb1621b1c5e0000bf07f34a7
KsKays <kapom0808@gmail.com>
2025-06-17 12:53:32 +0700

    update : Categories.jsx , ShoppingCart.jsx

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,32 +1,43 @@
 import { useEffect, useState } from "react";
 import CategorieService from "../services/categorie.service";
+import { useNavigate } from "react-router";
+import MainCategorieService from"../services/mainCategory.service";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCategory = async () => {
       try {
-        const response = await CategorieService.getAllCategorie();
+        const response = await MainCategorieService.getAllMainCategories();
         setCategories(response.data);
       } catch (error) {
-        console.error("เกิดข้อผิดพลาดในการเรียกหมวดหมู่: ", error);
+        console.error("Error fetching categories:", error);
       }
     };
     fetchCategory();
   }, []);
-  
+
+  const handleClickCategory = (categoryId) => {
+    navigate(`/shoppingpost?category=${categoryId}`, );
+  };
+
   return (
     <div className="section-container mt-22">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {categories.map((category) => (
-          <div key={category._id} className="categories-card">
-            {category.name}
+          <div
+            key={category._id}
+            className="categories-card cursor-pointer"
+            onClick={() => handleClickCategory(category._id)}
+          >
             <img
               className="w-16 h-16 object-cover rounded-md"
               src={category.image}
               alt={category.name}
             />
+            <p className="text-sm text-center">{category.name}</p>
           </div>
         ))}
       </div>
