b2f8b9ce320ff9d0e1cac4e78bac9f0ea6d61a44
YanakornS <654259002@webmail.npru.ac.th>
2025-06-18 11:09:00 +0700

    update : remove sensitive information from .env and add spacing in ProfileAndMenu component

--- frontend/src/components/ProfileAndMenu.jsx
+++ frontend/src/components/ProfileAndMenu.jsx
@@ -12,7 +12,8 @@ import { LuPlus } from "react-icons/lu";
 
 const ProfileAndMenu = () => {
   //ใช้ฟังก์ชันgetUserคืนข้อมูลผู้ใช้ละเอียดกว่า user
-  const { user, logout, getUser } = useContext(AuthContext);
+  const { user, logout, getUser } = useContext(AuthContext); 
+  //คือการ ดึงค่าตัวแปรหรือฟังก์ชันที่อยู่ใน Context มาใช้งานใน Component นั้นๆ ผ่าน useContext() ซึ่งทำงานแบบ Destructuring (แยกค่าออกจาก object) จาก AuthContext
 
   //ใช้เพื่อนำผู้ใช้ไปยังหน้า /post และส่ง breadcrumb เพื่อบอกทางกลับ
   const handleGoToPost = () => {
