a6cd1c8f5cc8a2bbb0fe059ca5795882380de780
YanakornS <654259002@webmail.npru.ac.th>
2025-07-02 10:11:07 +0700

    UpdateAdminSold

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -63,53 +63,10 @@ const PostSchema = new Schema(
       default: null,
     },
   },
-<<<<<<< HEAD
   {
     timestamps: true,
   }
 );
-=======
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  condition: {
-    type: String,
-    enum: ["มือสองสภาพดี", "มือสองสภาพพอใช้"],
-    required: true,
-  },
-  postPaymentType: {
-    type: String,
-    enum: ["Free", "Paid"],
-    default: "Free",
-  },
-  status: {
-    type: String,
-    enum: ["pending_review", "approved", "needs_revision", "rejected","sold"],
-    default: "pending_review",
-  },
-  modNote: {
-    type: String,
-    default: null,
-  
-  },
-  isSold: {
-    type: Boolean,
-    default: false,
-  },
-  buyer: {
-  type: Schema.Types.ObjectId,
-  ref: "User",
-  default: null,
-},
-},{
-  timestamps: true,
-});
->>>>>>> AdminAndSold
 
 const PostModel = model("Post", PostSchema);
 module.exports = PostModel;
